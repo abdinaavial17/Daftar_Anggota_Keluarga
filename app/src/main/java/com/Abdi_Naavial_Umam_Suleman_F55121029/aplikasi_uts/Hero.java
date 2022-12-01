597f426e2f1b399e25905374a2a96a6fb401a7c4
abdinaavial17 <abdinaavial17@gmail.com>
2022-12-01 17:52:07 +0800

    Initial commit

---
+++ app/src/main/java/com/Abdi_Naavial_Umam_Suleman_F55121029/aplikasi_uts/Hero.java
@@ -0,0 +1,31 @@
+package com.Abdi_Naavial_Umam_Suleman_F55121029.aplikasi_uts;
+
+public class Hero {
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private int photo;
+    private String name;
+    private String description;
+}
